36bcdaaa52f4604f802aea83e3e019f6c662f52a
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-07-26 19:46:11 +0530

    Create Loading.js

---
+++ React-Js/Toggle-Content/Loading.js
@@ -0,0 +1,9 @@
+function Loading() {
+  return <h1>Screen is loading {...} </h1>;
+}
+export default Loading;
+////
+function Loaded() {
+  return <h1>Screen is loaded {...} </h1>;
+}
+export default Loaded;
